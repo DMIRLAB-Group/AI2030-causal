a4fddbba5b7d5325c8fc80f3e064b8b13e962516
LanKing-220 <zsjyxy@qq.com>
2026-04-13 09:49:21 +0800

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            // 自动导入 Vue 相关函数，以及 Element Plus 的按需 API
-            imports: ['vue', 'vue-router', 'pinia'],
-            dts: true,
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            // 自动按需注册组件
-            resolvers: [ElementPlusResolver({ importStyle: 'css' })],
-            dts: true,
-        }),
-    ],
-    server: {
-        port: 3000,
-    },
-})
+  base: '/dmir/',
+  plugins: [
+    vue(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，以及 Element Plus 的按需 API
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: true,
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      // 自动按需注册组件
+      resolvers: [ElementPlusResolver({ importStyle: 'css' })],
+      dts: true,
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+});
